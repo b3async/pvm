ba8ef9aa23bff3d0fe2b288dcb0be1a78fb233e1
Samuel Oliveira <samueldevbr@gmail.com>
2022-08-28 00:00:03 -0300

    using lenaris discovery system to fetch os details on cli

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use lenaris::OperatingSystem;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let operating_system = OperatingSystem::discover()?;
+
+    println!("{operating_system:?}");
+
+    Ok(())
 }
