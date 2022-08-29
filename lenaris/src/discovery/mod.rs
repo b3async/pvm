5f6408e92a4cf14a0ab4175d53ff6e9e290e2695
Samuel Oliveira <samueldevbr@gmail.com>
2022-08-29 20:59:30 -0300

    refactoring discovery module to be unit testable

--- lenaris/src/discovery/mod.rs
+++ lenaris/src/discovery/mod.rs
@@ -1,126 +1,124 @@
-use crate::error::Error;
+mod services;
+pub use services::SysInfo;
 
 #[derive(Debug, PartialEq, Eq)]
-pub enum Distro {
+pub enum DistroID {
     Arch,
     Debian,
     Rhel,
 }
 
-impl TryFrom<Option<String>> for Distro {
-    type Error = Error;
+impl TryFrom<String> for DistroID {
+    type Error = crate::error::Error;
 
-    fn try_from(value: Option<String>) -> Result<Self, Self::Error> {
-        if let Some(value) = value {
-            let distro = value.to_ascii_lowercase();
-            let distro = distro.as_str();
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        let value = value.to_lowercase();
 
-            match distro {
-                "arch" => return Ok(Distro::Arch),
-                "debian" => return Ok(Distro::Debian),
-                distro => return Err(Error::UnsupportedDistro(distro.to_owned())),
-            }
-        }
-
-        Err(Error::Failed("unable to discover distro".to_owned()))
-    }
-}
-
-#[derive(Debug)]
-pub enum OperatingSystem {
-    Linux(Distro),
-    Darwin,
-}
-
-impl TryFrom<Result<String, sys_info::Error>> for OperatingSystem {
-    type Error = Error;
-
-    fn try_from(value: Result<String, sys_info::Error>) -> Result<Self, Self::Error> {
-        let os_type = match value {
-            Ok(os_type) => os_type,
-            Err(err) => {
-                return Err(Error::Failed(format!(
-                    "unable to discover os type: {}",
-                    err
-                )))
-            }
-        };
-
-        let os_type = os_type.to_lowercase();
-        let os_type = os_type.as_str();
-
-        match os_type {
-            "linux" => {
-                let os_release = match sys_info::linux_os_release() {
-                    Ok(release) => release,
-                    Err(err) => {
-                        return Err(Error::Failed(format!(
-                            "unable to fetch linux release: {}",
-                            err
-                        )))
-                    }
-                };
-
-                let distro = Distro::try_from(os_release.id_like)?;
-
-                Ok(Self::Linux(distro))
-            }
-            "darwin" => Ok(Self::Darwin),
-            os_type => Err(Error::UnsupportedSystem(os_type.to_owned())),
+        match value.as_str() {
+            "debian" => Ok(Self::Debian),
+            "arch" => Ok(Self::Arch),
+            "rhel" | "rhel fedora" => Ok(Self::Rhel),
+            distro_id => Err(crate::error::Error::UnsupportedDistro(distro_id.to_owned())),
         }
     }
 }
 
-impl OperatingSystem {
-    pub fn discover() -> Result<Self, Error> {
-        OperatingSystem::try_from(sys_info::os_type())
+#[derive(Debug, PartialEq, Eq)]
+pub enum VendorID {
+    Linux,
+    MacOS,
+}
+
+impl TryFrom<String> for VendorID {
+    type Error = crate::error::Error;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        let value = value.to_lowercase();
+
+        match value.as_str() {
+            "linux" => Ok(Self::Linux),
+            "darwin" => Ok(Self::MacOS),
+            vendor_id => Err(crate::error::Error::UnsupportedSystem(vendor_id.to_owned())),
+        }
+    }
+}
+
+pub trait DiscoveryService {
+    type Error;
+
+    fn get_distro_id() -> Result<String, Self::Error>;
+    fn get_vendor_id() -> Result<String, Self::Error>;
+}
+
+pub struct Vendor(pub VendorID, pub Option<DistroID>);
+
+impl Vendor {
+    pub fn discover<DS>() -> Result<Self, crate::error::Error>
+    where
+        DS: DiscoveryService<Error = crate::error::Error>,
+    {
+        let vendor_id = <DS>::get_vendor_id()?;
+        let vendor_id = VendorID::try_from(vendor_id)?;
+
+        match vendor_id {
+            VendorID::Linux => {
+                let distro = <DS>::get_distro_id()?;
+                let distro = DistroID::try_from(distro)?;
+
+                Ok(Self {
+                    0: vendor_id,
+                    1: Some(distro),
+                })
+            }
+            VendorID::MacOS => Ok(Self {
+                0: vendor_id,
+                1: None,
+            }),
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::error::Error;
+
     use super::*;
 
     #[test]
-    fn can_discover_os() {
-        fn mock_os(os: &str, err: Option<sys_info::Error>) -> Result<String, sys_info::Error> {
-            if let Some(err) = err {
-                return Err(err);
-            }
+    fn can_convert_string_to_vendor_id() {
+        let darwin = "Darwin".to_owned();
+        let linux = "Linux".to_owned();
 
-            Ok(os.to_owned())
-        }
+        assert_eq!(VendorID::try_from(darwin).unwrap(), VendorID::MacOS);
+        assert_eq!(VendorID::try_from(linux).unwrap(), VendorID::Linux);
 
-        #[cfg(target_os = "linux")]
-        {
-            let linux_os = OperatingSystem::try_from(mock_os("Linux", None));
-            assert!(linux_os.map_err(|err| eprintln!("{err}")).is_ok());
-        }
+        let windows = VendorID::try_from("Windows".to_owned());
 
-        #[cfg(target_os = "macos")]
-        {
-            let mac_os = OperatingSystem::try_from(mock_os("Darwin", None));
-            assert!(mac_os.map_err(|err| eprintln!("{err}")).is_ok());
-        }
-
-        let windows_os = OperatingSystem::try_from(mock_os("Windows", None));
-        assert!(windows_os.is_err());
-
-        let unknown_os =
-            OperatingSystem::try_from(mock_os("", Some(sys_info::Error::UnsupportedSystem)));
-
-        assert!(unknown_os.is_err());
+        assert!(windows.is_err());
+        assert_eq!(
+            windows.err().unwrap(),
+            Error::UnsupportedSystem("windows".to_owned())
+        );
     }
 
     #[test]
-    fn can_discover_distro() {
-        let arch = Distro::try_from(Some("arch".to_owned())).unwrap();
-        let debian = Distro::try_from(Some("debian".to_owned())).unwrap();
-        let rhel = Distro::try_from(Some("rhel fedora".to_owned()));
+    fn can_convert_string_to_distro_id() {
+        let debian = DistroID::try_from("Debian".to_owned());
+        let arch = DistroID::try_from("Arch".to_owned());
+        let fedora = DistroID::try_from("Rhel fedora".to_owned());
+        let rhel = DistroID::try_from("Rhel".to_owned());
 
-        assert_eq!(Distro::Arch, arch);
-        assert_eq!(Distro::Debian, debian);
+        assert_eq!(debian.unwrap(), DistroID::Debian);
+        assert_eq!(arch.unwrap(), DistroID::Arch);
+        assert_eq!(fedora.unwrap(), DistroID::Rhel);
+        assert_eq!(rhel.unwrap(), DistroID::Rhel);
 
-        assert!(rhel.is_err());
+        let solaris = DistroID::try_from("Solaris".to_owned());
+
+        assert!(solaris.is_err());
+        assert_eq!(
+            solaris.err().unwrap(),
+            Error::UnsupportedDistro("solaris".to_owned())
+        );
     }
 }
